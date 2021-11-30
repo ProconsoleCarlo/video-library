9fe707a1f9cf3be82389c76b2974c3a138fa2209
Carlo Bobba <carlo.bobba@outlook.it>
2021-11-30 22:37:05 +0100

    Add test

---
+++ video-library-rest/src/test/java/it/proconsole/library/video/rest/Fixtures.java
@@ -0,0 +1,43 @@
+package it.proconsole.library.video.rest;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.lang.NonNull;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+public class Fixtures {
+  private static final ObjectMapper objectMapper = new ObjectMapper()
+          .findAndRegisterModules()
+          .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+
+  @NonNull
+  public static String readFromClasspath(@NonNull String path) {
+    try {
+      return new String(Fixtures.class.getResourceAsStream(path).readAllBytes(), StandardCharsets.UTF_8);
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  @NonNull
+  public static <T> T readFromClasspath(@NonNull String path, @NonNull Class<T> clazz) {
+    try {
+      return objectMapper
+              .readValue(Fixtures.class.getResourceAsStream(path), clazz);
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  public static <T> T readFromClasspath(@NonNull String path, @NonNull TypeReference<T> typeReference) {
+    try {
+      return objectMapper
+              .readValue(Fixtures.class.getResourceAsStream(path), typeReference);
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
+  }
+}
---
+++ video-library-rest/src/test/java/it/proconsole/library/video/rest/controller/JdbcControllerTest.java
@@ -0,0 +1,46 @@
+package it.proconsole.library.video.rest.controller;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import it.proconsole.library.video.adapter.jdbc.model.Film;
+import it.proconsole.library.video.adapter.jdbc.repository.FilmRepository;
+import it.proconsole.library.video.rest.Fixtures;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.List;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup;
+
+@ExtendWith(MockitoExtension.class)
+class JdbcControllerTest {
+  private static final String FILMS_JSON = "/it/proconsole/library/video/rest/controller/films.json";
+
+  @Mock
+  private FilmRepository filmRepository;
+
+  private MockMvc mvc;
+
+  @BeforeEach
+  void setUp() {
+    mvc = standaloneSetup(new JdbcController(filmRepository)).build();
+  }
+
+  @Test
+  void getFilms() throws Exception {
+    List<Film> films = Fixtures.readFromClasspath(FILMS_JSON, new TypeReference<>() {
+    });
+    when(filmRepository.findAll()).thenReturn(films);
+
+    mvc.perform(get("/jdbc/films"))
+            .andExpect(status().isOk())
+            .andExpect(content().json(Fixtures.readFromClasspath(FILMS_JSON)));
+  }
+}

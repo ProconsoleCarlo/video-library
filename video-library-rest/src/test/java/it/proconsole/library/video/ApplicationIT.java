ab3fafd8f8e31c06bd11f9d1c411aa2d22290751
Carlo Bobba <carlo.bobba@outlook.it>
2021-12-01 12:13:18 +0100

    Add smoke test

---
+++ video-library-rest/src/test/java/it/proconsole/library/video/ApplicationIT.java
@@ -0,0 +1,15 @@
+package it.proconsole.library.video;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(classes = Application.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class ApplicationIT {
+  @Test
+  void main() { //NOSONAR
+
+  }
+}

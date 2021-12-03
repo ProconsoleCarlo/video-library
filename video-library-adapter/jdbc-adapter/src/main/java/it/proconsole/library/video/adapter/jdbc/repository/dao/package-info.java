21ddbaec3c738f608e28523e8d4e94513e2cd210
Carlo Bobba <carlo.bobba@outlook.it>
2021-12-03 14:51:47 +0100

    Daos are not nullable

---
+++ video-library-adapter/jdbc-adapter/src/main/java/it/proconsole/library/video/adapter/jdbc/repository/dao/package-info.java
@@ -0,0 +1,6 @@
+@NonNullApi
+@NonNullFields
+package it.proconsole.library.video.adapter.jdbc.repository.dao;
+
+import org.springframework.lang.NonNullApi;
+import org.springframework.lang.NonNullFields;

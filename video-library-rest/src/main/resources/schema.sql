cda6410f4a289bfe9bddecc7967fe6326f3e6def
ProconsoleCarlo <60896784+ProconsoleCarlo@users.noreply.github.com>
2021-12-10 20:50:34 +0100

    [VL-8] Use a dummy H2 DB on local during development (#13)

---
+++ video-library-rest/src/main/resources/schema.sql
@@ -0,0 +1,38 @@
+drop table IF EXISTS `genre`;
+create TABLE `genre`
+(
+    `id`    int          NOT NULL AUTO_INCREMENT,
+    `value` varchar(255) NOT NULL,
+    PRIMARY KEY (`id`)
+);
+
+drop table IF EXISTS `film`;
+create TABLE `film`
+(
+    `id`    int          NOT NULL AUTO_INCREMENT,
+    `title` varchar(255) NOT NULL,
+    `year`  year         NOT NULL,
+    PRIMARY KEY (`id`)
+);
+
+drop table IF EXISTS `film_genres`;
+create TABLE `film_genres`
+(
+    `film_id`  int NOT NULL,
+    `genre_id` int NOT NULL,
+    PRIMARY KEY (`film_id`, `genre_id`),
+    CONSTRAINT `fk_film_genre_film1` FOREIGN KEY (`film_id`) REFERENCES `film` (`id`),
+    CONSTRAINT `fk_film_genre_genre1` FOREIGN KEY (`genre_id`) REFERENCES `genre` (`id`)
+);
+
+drop table IF EXISTS `film_review`;
+create TABLE `film_review`
+(
+    `id`      int      NOT NULL AUTO_INCREMENT,
+    `date`    datetime NOT NULL,
+    `rating`  tinyint  NOT NULL,
+    `detail`  text,
+    `film_id` int      NOT NULL,
+    PRIMARY KEY (`id`),
+    CONSTRAINT `fk_film_review_film1` FOREIGN KEY (`film_id`) REFERENCES `film` (`id`)
+);

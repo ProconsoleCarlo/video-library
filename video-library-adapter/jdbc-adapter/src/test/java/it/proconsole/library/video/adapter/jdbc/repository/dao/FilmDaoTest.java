2d5e21b03915175c53dd8dc9abc73ccaf77c45b5
Carlo Bobba <carlo.bobba@outlook.it>
2021-12-12 12:21:34 +0100

    [VL-1] FilmReviewDao and test are complete

--- video-library-adapter/jdbc-adapter/src/test/java/it/proconsole/library/video/adapter/jdbc/repository/dao/FilmDaoTest.java
+++ video-library-adapter/jdbc-adapter/src/test/java/it/proconsole/library/video/adapter/jdbc/repository/dao/FilmDaoTest.java
@@ -2,37 +2,26 @@ package it.proconsole.library.video.adapter.jdbc.repository.dao;
 
 import it.proconsole.library.video.adapter.ApplicationConfig;
 import it.proconsole.library.video.adapter.jdbc.repository.entity.FilmEntity;
-import it.proconsole.library.video.core.Fixtures;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Profile;
 
 import javax.sql.DataSource;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
 @SpringBootTest(classes = ApplicationConfig.class)
 @Profile("test")
-class FilmDaoTest {
-  private static final String FILMS_JSON = "/it/proconsole/library/video/adapter/model/filmEntities.json";
-
+class FilmDaoTest extends DatabaseDaoTest<FilmEntity> {
   @Autowired
   private DataSource dataSource;
-
-  private FilmDao dao;
 
   @BeforeEach
   void setUp() {
     dao = new FilmDao(dataSource);
   }
 
-  @Test
-  void findAll() {
-    var current = dao.findAll();
-    var expected = Fixtures.readListFromClasspath(FILMS_JSON, FilmEntity.class);
-
-    assertEquals(expected, current);
+  @Override
+  FilmEntity anEntity() {
+    return new FilmEntity(1L, "Film title", 2020);
   }
 }

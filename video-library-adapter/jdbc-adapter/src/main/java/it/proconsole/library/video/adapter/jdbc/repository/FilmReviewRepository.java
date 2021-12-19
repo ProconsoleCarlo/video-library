cc122f3d151bb543c20151980007f4a99a19ad17
Carlo Bobba <carlo.bobba@outlook.it>
2021-12-19 20:27:14 +0100

    [VL-1] Add FilmReviewRepositoryTest

--- video-library-adapter/jdbc-adapter/src/main/java/it/proconsole/library/video/adapter/jdbc/repository/FilmReviewRepository.java
+++ video-library-adapter/jdbc-adapter/src/main/java/it/proconsole/library/video/adapter/jdbc/repository/FilmReviewRepository.java
@@ -1,11 +1,8 @@
 package it.proconsole.library.video.adapter.jdbc.repository;
 
 import it.proconsole.library.video.adapter.jdbc.repository.dao.FilmReviewDao;
-import it.proconsole.library.video.adapter.jdbc.repository.entity.FilmEntity;
 import it.proconsole.library.video.adapter.jdbc.repository.entity.FilmReviewEntity;
 import it.proconsole.library.video.core.model.FilmReview;
-
-import java.util.List;
 
 public class FilmReviewRepository {
   private final FilmReviewDao filmReviewDao;
@@ -15,13 +12,6 @@ public class FilmReviewRepository {
   }
 
   public FilmReview save(FilmReview review) {
-    var filmReviewEntity = FilmReviewEntity.fromDomain(review);
-    return filmReviewDao.save(filmReviewEntity).toDomain();
-  }
-
-  private List<FilmReview> retrieveReviewsFor(FilmEntity film) {
-    return filmReviewDao.findByFilmId(film.id())
-            .stream().map(FilmReviewEntity::toDomain)
-            .toList();
+    return filmReviewDao.save(FilmReviewEntity.fromDomain(review)).toDomain();
   }
 }
---
+++ video-library-adapter/jdbc-adapter/src/test/java/it/proconsole/library/video/adapter/jdbc/repository/FilmReviewRepositoryTest.java
@@ -0,0 +1,43 @@
+package it.proconsole.library.video.adapter.jdbc.repository;
+
+import it.proconsole.library.video.adapter.jdbc.repository.dao.FilmReviewDao;
+import it.proconsole.library.video.core.model.FilmReview;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
+import org.springframework.test.context.jdbc.Sql;
+
+import javax.sql.DataSource;
+import java.time.LocalDateTime;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@JdbcTest
+@Sql({"/schema.sql", "/data.sql"})
+class FilmReviewRepositoryTest {
+  @Autowired
+  private DataSource dataSource;
+
+  private FilmReviewRepository repository;
+
+  @BeforeEach
+  void setUp() {
+    repository = new FilmReviewRepository(new FilmReviewDao(dataSource));
+  }
+
+  @Test
+  void save() {
+    var review = new FilmReview(1L, LocalDateTime.now(), 8, "A review", 1L);
+
+    var savedReview = repository.save(review);
+
+    assertEquals(review, savedReview);
+
+    var reviewToUpdate = new FilmReview(1L, LocalDateTime.now(), 8, "A review updated", 1L);
+
+    var updatedReview = repository.save(reviewToUpdate);
+
+    assertEquals(reviewToUpdate, updatedReview);
+  }
+}

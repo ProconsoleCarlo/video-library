b46fbf78c6369ee7a37069b44c68261b45b832b6
Carlo Bobba <carlo.bobba@outlook.it>
2021-12-20 21:47:59 +0100

    [VL-1] Remove unused file

--- video-library-rest/src/main/java/it/proconsole/library/video/rest/model/FilmView.java
+++
@@ -1,9 +0,0 @@
-package it.proconsole.library.video.rest.model;
-
-import org.springframework.lang.Nullable;
-
-public record FilmView(@Nullable Long id,
-                       String title,
-                       int year
-) {
-}
--- video-library-rest/src/main/java/it/proconsole/library/video/rest/model/package-info.java
+++
@@ -1,6 +0,0 @@
-@NonNullApi
-@NonNullFields
-package it.proconsole.library.video.rest.model;
-
-import org.springframework.lang.NonNullApi;
-import org.springframework.lang.NonNullFields;

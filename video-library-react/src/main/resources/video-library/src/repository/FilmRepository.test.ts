dac7aa66ce96d23dbc70080dabc19274a250ef07
Carlo Bobba <carlo.bobba@outlook.it>
2021-12-30 11:26:59 +0100

    [VL-14] Improve tests

--- video-library-react/src/main/resources/video-library/src/repository/FilmRepository.test.ts
+++ video-library-react/src/main/resources/video-library/src/repository/FilmRepository.test.ts
@@ -1,44 +1,49 @@
-import { filmRepository } from './FilmRepository';
+import assert from 'assert';
 import fetchMockJest from 'fetch-mock-jest';
-import { deepStrictEqual } from 'assert';
 import { existentFilms, insertFilms } from '../mock/FilmsMock';
+import { protocols } from '../model/Protocols';
+import { filmRepository } from './FilmRepository';
 
 describe('FilmRepository', () => {
-	const repository = filmRepository('jpa');
+	protocols.forEach(protocol => {
+		describe(`when protocol is ${protocol}`, () => {
+			const repository = filmRepository(protocol);
 
-	test('get', async () => {
-		fetchMockJest.getOnce(
-			(url: string) => url === `/jpa/films`,
-			existentFilms
-		);
+			test('get', async () => {
+				fetchMockJest.getOnce(
+					(url: string) => url === `/${protocol}/films`,
+					existentFilms
+				);
 
-		await repository.get()
-			.then(it => expect(it).toStrictEqual(existentFilms));
-	});
+				await repository.get()
+					.then(it => expect(it).toStrictEqual(existentFilms));
+			});
 
-	test('update', async () => {
-		fetchMockJest.postOnce(
-			(url: string, opts: Request | RequestInit) => {
-				deepStrictEqual(JSON.parse(opts.body!.toString()), existentFilms);
-				return url === `/jpa/films`;
-			},
-			existentFilms
-		);
+			test('update', async () => {
+				fetchMockJest.postOnce(
+					(url: string, opts: Request | RequestInit) => {
+						assert.deepStrictEqual(JSON.parse(opts.body!.toString()), existentFilms);
+						return url === `/${protocol}/films`;
+					},
+					existentFilms
+				);
 
-		await repository.update(existentFilms)
-			.then(it => expect(it).toStrictEqual(existentFilms));
-	});
+				await repository.update(existentFilms)
+					.then(it => expect(it).toStrictEqual(existentFilms));
+			});
 
-	test('insert', async () => {
-		fetchMockJest.putOnce(
-			(url: string, opts: Request | RequestInit) => {
-				deepStrictEqual(JSON.parse(opts.body!.toString()), insertFilms);
-				return url === `/jpa/films`;
-			},
-			existentFilms
-		);
+			test('insert', async () => {
+				fetchMockJest.putOnce(
+					(url: string, opts: Request | RequestInit) => {
+						assert.deepStrictEqual(JSON.parse(opts.body!.toString()), insertFilms);
+						return url === `/${protocol}/films`;
+					},
+					existentFilms
+				);
 
-		await repository.insert(insertFilms)
-			.then(it => expect(it).toStrictEqual(existentFilms));
+				await repository.insert(insertFilms)
+					.then(it => expect(it).toStrictEqual(existentFilms));
+			});
+		});
 	});
 });
--- video-library-react/src/main/resources/video-library/src/repository/FilmReviewRepository.test.ts
+++ video-library-react/src/main/resources/video-library/src/repository/FilmReviewRepository.test.ts
@@ -1,36 +1,41 @@
+import assert from 'assert';
 import fetchMockJest from 'fetch-mock-jest';
-import { filmReviewRepository } from './FilmReviewRepository';
-import { deepStrictEqual } from 'assert';
 import { existentFilmReview, insertFilmReview } from '../mock/FilmReviewMock';
+import { protocols } from '../model/Protocols';
+import { filmReviewRepository } from './FilmReviewRepository';
 
 describe('FilmReviewRepository', () => {
 	const FILM_ID = 1;
 
-	const repository = filmReviewRepository('jpa');
+	protocols.forEach(protocol => {
+		describe(`when protocol is ${protocol}`, () => {
+			const repository = filmReviewRepository(protocol);
 
-	test('update', async () => {
-		fetchMockJest.postOnce(
-			(url: string, opts: Request | RequestInit) => {
-				deepStrictEqual(JSON.parse(opts.body!.toString()), existentFilmReview);
-				return url === `/jpa/review/${FILM_ID}`;
-			},
-			existentFilmReview
-		);
+			test('update', async () => {
+				fetchMockJest.postOnce(
+					(url: string, opts: Request | RequestInit) => {
+						assert.deepStrictEqual(JSON.parse(opts.body!.toString()), existentFilmReview);
+						return url === `/${protocol}/review/${FILM_ID}`;
+					},
+					existentFilmReview
+				);
 
-		await repository.update(existentFilmReview, FILM_ID)
-			.then(it => expect(it).toStrictEqual(existentFilmReview));
-	});
+				await repository.update(existentFilmReview, FILM_ID)
+					.then(it => expect(it).toStrictEqual(existentFilmReview));
+			});
 
-	test('insert', async () => {
-		fetchMockJest.putOnce(
-			(url: string, opts: Request | RequestInit) => {
-				deepStrictEqual(JSON.parse(opts.body!.toString()), insertFilmReview);
-				return url === `/jpa/review/${FILM_ID}`;
-			},
-			existentFilmReview
-		);
+			test('insert', async () => {
+				fetchMockJest.putOnce(
+					(url: string, opts: Request | RequestInit) => {
+						assert.deepStrictEqual(JSON.parse(opts.body!.toString()), insertFilmReview);
+						return url === `/${protocol}/review/${FILM_ID}`;
+					},
+					existentFilmReview
+				);
 
-		await repository.insert(insertFilmReview, FILM_ID)
-			.then(it => expect(it).toStrictEqual(existentFilmReview));
+				await repository.insert(insertFilmReview, FILM_ID)
+					.then(it => expect(it).toStrictEqual(existentFilmReview));
+			});
+		});
 	});
 });

328431c369bed5417ac0d0d2f6ceda5bf3ed28e2
Carlo Bobba <carlo.bobba@outlook.it>
2022-02-06 20:28:22 +0100

    [VL-12] Add some logs

--- video-library-adapter/jpa-adapter/src/main/java/it/proconsole/library/video/adapter/jpa/repository/JpaFilmRepository.java
+++ video-library-adapter/jpa-adapter/src/main/java/it/proconsole/library/video/adapter/jpa/repository/JpaFilmRepository.java
@@ -4,6 +4,7 @@ import it.proconsole.library.video.adapter.jpa.repository.adapter.FilmAdapter;
 import it.proconsole.library.video.adapter.jpa.repository.crud.FilmCrudRepository;
 import it.proconsole.library.video.core.model.Film;
 import it.proconsole.library.video.core.repository.FilmRepository;
+import it.proconsole.library.video.core.repository.Operation;
 import it.proconsole.library.video.core.repository.Protocol;
 
 import java.util.List;
@@ -24,13 +25,18 @@ public class JpaFilmRepository implements FilmRepository {
 
   @Override
   public List<Film> findAll() {
-    return filmAdapter.toDomain(filmCrudRepository.findAll());
+    logStart(protocol(), Operation.FIND_ALL);
+    var films = filmAdapter.toDomain(filmCrudRepository.findAll());
+    logEnd(protocol(), Operation.FIND_ALL, films.size());
+    return films;
   }
 
   @Override
   public List<Film> saveAll(List<Film> films) {
+    logStart(protocol(), Operation.SAVE_ALL, films.size());
     var entities = filmAdapter.fromDomain(films);
-    var saved = filmCrudRepository.saveAll(entities);
-    return filmAdapter.toDomain(saved);
+    var savedFilms = filmCrudRepository.saveAll(entities);
+    logEnd(protocol(), Operation.SAVE_ALL, savedFilms.size());
+    return filmAdapter.toDomain(savedFilms);
   }
 }
--- video-library-core/src/main/java/it/proconsole/library/video/core/repository/FilmRepository.java
+++ video-library-core/src/main/java/it/proconsole/library/video/core/repository/FilmRepository.java
@@ -1,11 +1,27 @@
 package it.proconsole.library.video.core.repository;
 
 import it.proconsole.library.video.core.model.Film;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.List;
 
 public interface FilmRepository extends ByProtocolRepository {
+  Logger logger = LoggerFactory.getLogger(FilmRepository.class);
+
   List<Film> findAll();
 
   List<Film> saveAll(List<Film> films);
+
+  default void logStart(Protocol protocol, Operation operation) {
+    logger.info("%s - %s request received".formatted(protocol, operation));
+  }
+
+  default void logStart(Protocol protocol, Operation operation, int inputs) {
+    logger.info("%s - %s request received with %d values".formatted(protocol, operation, inputs));
+  }
+
+  default void logEnd(Protocol protocol, Operation operation, int results) {
+    logger.info("%s - %s request completed with %d results".formatted(protocol, operation, results));
+  }
 }

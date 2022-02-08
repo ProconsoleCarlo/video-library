fdeda2d12252da8be732c500f4357152278c6fe5
ProconsoleCarlo <60896784+ProconsoleCarlo@users.noreply.github.com>
2022-02-08 21:24:16 +0100

    [VL-16] Upgrade to Spring 3.0.0-M1 (#31)

--- video-library-adapter/jpa-adapter/src/main/java/it/proconsole/library/video/adapter/jpa/model/FilmReviewEntity.java
+++ video-library-adapter/jpa-adapter/src/main/java/it/proconsole/library/video/adapter/jpa/model/FilmReviewEntity.java
@@ -1,20 +1,20 @@
 package it.proconsole.library.video.adapter.jpa.model;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Index;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.Lob;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import org.hibernate.Hibernate;
 import org.springframework.lang.Nullable;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Index;
-import javax.persistence.JoinColumn;
-import javax.persistence.Lob;
-import javax.persistence.ManyToOne;
-import javax.persistence.Table;
 import java.time.LocalDateTime;
 import java.util.Objects;
 

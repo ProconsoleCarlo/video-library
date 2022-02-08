c1f7528408655e3b4df8ddb9274d82c4130eae99
Carlo Bobba <carlo.bobba@outlook.it>
2022-02-08 21:22:41 +0100

    [VL-16] Upgrade to Spring 3.0.0-M1

--- video-library-adapter/jpa-adapter/src/main/java/it/proconsole/library/video/adapter/jpa/model/FilmEntity.java
+++ video-library-adapter/jpa-adapter/src/main/java/it/proconsole/library/video/adapter/jpa/model/FilmEntity.java
@@ -1,19 +1,19 @@
 package it.proconsole.library.video.adapter.jpa.model;
 
 import com.fasterxml.jackson.annotation.JsonManagedReference;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 import org.hibernate.Hibernate;
 
-import javax.persistence.CascadeType;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.JoinTable;
-import javax.persistence.ManyToMany;
-import javax.persistence.OneToMany;
-import javax.persistence.Table;
 import java.util.List;
 import java.util.Objects;
 
--- video-library-adapter/jpa-adapter/src/main/java/it/proconsole/library/video/adapter/jpa/model/GenreEntity.java
+++ video-library-adapter/jpa-adapter/src/main/java/it/proconsole/library/video/adapter/jpa/model/GenreEntity.java
@@ -1,16 +1,16 @@
 package it.proconsole.library.video.adapter.jpa.model;
 
 import it.proconsole.library.video.core.model.GenreEnum;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import org.hibernate.Hibernate;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
 import java.util.Objects;
 
 @Table(name = "genre")
